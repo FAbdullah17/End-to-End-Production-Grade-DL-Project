cbf26b90ecdae8ab1e22da8be5b7f0094e47e567
Fahad Abdullah <134834352+FAbdullah17@users.noreply.github.com>
2025-01-01 13:08:47 +0500

    Final Commit

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup , find_packages
 
 
 setup(
-    name="us_visa",
+    name="End-to-End-Production-Grade-DL-Project",
     version="0.0.0",
     packages=find_packages(),
-    author="Danyal",
-    author_email="danyal514@gmail.com"
+    author="FAbdullah17",
+    author_email="fahadai.co@gmail.com"
 )
